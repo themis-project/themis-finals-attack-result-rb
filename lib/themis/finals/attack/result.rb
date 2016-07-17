4d6cb712192cf922a7aa1a978e333fd16531bb9f
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-07-17 09:10:31 +0000

    Use Themis::Finals namespace

---
+++ lib/themis/finals/attack/result.rb
@@ -0,0 +1,31 @@
+require 'ruby-enum'
+
+module Themis
+  module Finals
+    module Attack
+      class Result
+        include Ruby::Enum
+
+        define :SUCCESS_FLAG_ACCEPTED, 0 # submitted flag has been accepted
+
+        define :ERR_GENERIC, 1 # generic error
+
+        define :ERR_INVALID_IDENTITY, 2 # the attacker does not appear to be a team
+        define :ERR_CONTEST_NOT_STARTED, 3 # contest has not been started yet
+        define :ERR_CONTEST_PAUSED, 4 # contest has been paused
+        define :ERR_CONTEST_COMPLETED, 5 # contest has been completed
+
+        define :ERR_INVALID_FORMAT, 6 # submitted data has invalid format
+
+        define :ERR_ATTEMPTS_LIMIT, 7 # attack attempts limit exceeded
+
+        define :ERR_FLAG_EXPIRED, 8 # submitted flag has expired
+        define :ERR_FLAG_YOURS, 9 # submitted flag belongs to the attacking team
+        define :ERR_FLAG_SUBMITTED, 10 # submitted flag has been accepted already
+        define :ERR_FLAG_NOT_FOUND, 11 # submitted flag has not been found
+
+        define :ERR_SERVICE_NOT_UP, 12 # the attacking team service is not up
+      end
+    end
+  end
+end
---
+++ themis-finals-attack-result.gemspec
@@ -0,0 +1,19 @@
+Gem::Specification.new do |s|
+  s.name = 'themis-finals-attack-result'
+  s.version = '1.0.0'
+  s.date = '2016-07-09'
+  s.summary = 'Themis::Finals::Attack::Result constants'
+  s.description = 'Themis::Finals::Attack::Result constants'
+  s.authors = ['Alexander Pyatkin']
+  s.email = 'aspyatkin@gmail.com'
+  s.files = ['lib/themis/finals/attack/result.rb']
+  s.homepage = 'http://github.com/aspyatkin/themis-finals-attack-result'
+  s.license = 'MIT'
+
+  s.required_ruby_version = '>= 2.0'
+
+  s.add_dependency 'ruby-enum'
+
+  s.add_development_dependency 'bundler'
+  s.add_development_dependency 'rake'
+end
